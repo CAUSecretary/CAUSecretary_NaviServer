c2c4a176fa8f78cf9934125f7b71684d8645b238
jobum97 <brian654321@naver.com>
2022-06-02 12:49:32 +0900

    fix

--- astar/src/main/java/com/example/astar/Routing/DBConnection.java
+++ astar/src/main/java/com/example/astar/Routing/DBConnection.java
@@ -11,7 +11,7 @@ import java.sql.SQLException;	//sql 쿼리의 예외처리를 위한 import
 public class DBConnection {
 
     private static final String DB_DRIVER_CLASS = "org.mariadb.jdbc.Driver";
-    private static final String DB_URL = "jjdbc:mysql://causecretarydb.cfoadhgjb3mf.ap-northeast-2.rds.amazonaws.com:3306/astar";
+    private static final String DB_URL = "jdbc:mysql://causecretarydb.cfoadhgjb3mf.ap-northeast-2.rds.amazonaws.com:3306/astar";
     private static final String DB_USERNAME = "causecretarydb";
     private static final String DB_PASSWORD = "smilecau1234";
 
